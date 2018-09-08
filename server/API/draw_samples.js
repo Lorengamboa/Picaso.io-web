df21c1658e98a521667abfa04ae6c3527188c243
Lorenzo Gamboa <lorenzo.gamboa@accenture.com>
2018-09-08 22:24:43 +0200

    Fixing paths

--- server/API/draw_samples.js
+++ server/API/draw_samples.js
@@ -7,7 +7,7 @@ var router = express.Router();
 const general_dictionary = require('../dictionary/general_en');
 const { rndValueArray, base64_encode } = require('../utils');
 
-const DRAW_SAMPLE_FOLDER = './draws/';
+const DRAW_SAMPLE_FOLDER = 'draws/';
 
 /**
  * @endpoint: /draws
